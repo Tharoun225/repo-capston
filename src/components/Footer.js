76c6345e11aba4bcac3dfa9575392bcb27ca95ce
Tharoun225 <haroun92traore@gmail.com>
2025-04-18 18:38:48 +0000

    ajout de nouveau code

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer(){
+    return(
+        <Footer className="footer">
+            <div className="footer-logo">
+                <img src="/images/logo.png" alt="Little Lemon Logo" />
+            </div>
+
+            <div className="footer-nav">
+                <div className="footer-section">
+                    <h4>Doormat Navigation</h4>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/menu">Menu</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/reservations">Reservations</a></li>
+                        </ul>
+                </div>
+
+                <div className="footer-section">
+                    <h4>Contact</h4>
+                        <ul>
+                            <li>123 Lemon St</li>
+                            <li>Chicago, IL</li>
+                            <li>(123) 456-7890</li>
+                            <li>hello@littlelemon.com</li>
+                        </ul>
+                </div>
+
+                <div className="footer-section">
+                    <h4>Social Media Links</h4>
+                        <ul>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">Twitter</a></li>
+                        </ul>
+                </div>
+            </div>
+        </Footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent(){
+    return(
+        <main>
+            <p>Ceci est le contenu principal de la page</p>
+        </main>
+    );
+}
+
+export default MainContent;
